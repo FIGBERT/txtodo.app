c7d16f022c6ed98f4fb6a93f77bf798d8f355cc9
therealFIGBERT <figbertwelner@gmail.com>
2020-04-27 14:32:16 -0700

    :lipstick: Increase thickness of DownloadIcon border in light mode

--- src/DownloadIcon.svelte
+++ src/DownloadIcon.svelte
@@ -6,7 +6,7 @@
     export let link
 </script>
 
-<a href={link} class="w-32 h-10 rounded-lg border border-2 border-{isDarkMode ? "white" : "black"} border-solid flex items-center" target="_blank" rel="noopener noreferrer">
+<a href={link} class="w-32 h-10 rounded-lg border border-{isDarkMode ? "2" : "4"} border-{isDarkMode ? "white" : "black"} border-solid flex items-center" target="_blank" rel="noopener noreferrer">
     <img src={isDarkMode ? srcPair[0] : srcPair[1]} alt={alt} class="w-5 my-1 mx-2"/>
     <p class="{isDarkMode ? "text-white" : "text-dark"} text-xs leading-snug">{label}</p>
 </a>
