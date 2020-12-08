6f80b65fc1b89625258db4cca74c12d4dd7a64d1
FIGBERT <figbert@figbert.com>
2020-12-08 14:57:47 -0800

    Update homepage for v3 release

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,6 @@
 </script>
 
 <header>
-	<section class="bg-black text-white text-center py-1 text-xs lg:text-lg">txtodo v2.0 out now!</section>
 	<h1 class="pl-5 lg:pl-10 font-hairline text-3xl lg:text-6xl">txtodo</h1>
 </header>
 <main class="w-full sm:w-1/2 lg:w-full mx-0 sm:mx-auto lg:mx-0 grid grid-cols-1 lg:grid-cols-2 grid-rows-4 lg:grid-rows-2 gap-6">
@@ -30,14 +29,14 @@
 		<div class="mt-8 flex flex-col lg:flex-row items-center lg:items-start">
 			<div class="inline-block align-middle mb-3 lg:mb-0 lg:mr-2"><DownloadIcon isDarkMode={isDarkMode} srcPair={["./apple-dark.svg", "./apple-light.svg"]} alt="apple logo" label="download the ios app" link="https://apps.apple.com/us/app/txtodo/id1504609185"/></div>
 			<div class="inline-block align-middle mb-3 lg:mb-0 lg:mr-2"><DownloadIcon isDarkMode={isDarkMode} srcPair={["./apple-dark.svg", "./apple-light.svg"]} alt="apple logo" label="download the macos app" link="https://apps.apple.com/us/app/txtodo-companion/id1517267304"/></div>
-			<div class="inline-block align-middle"><DownloadIcon isDarkMode={isDarkMode} srcPair={["./github-dark.svg", "./github-light.svg"]} alt="github logo" label="view my code" link="https://git.figbert.com/FIGBERT/txtodo.app"/></div>
+			<div class="inline-block align-middle"><DownloadIcon isDarkMode={isDarkMode} srcPair={["./github-dark.svg", "./github-light.svg"]} alt="github logo" label="view my code" link="https://git.figbert.com/FIGBERT/txtodo"/></div>
 		</div>
 	</section>
 	<img src={phoneImg} alt="txtodo running on an iPhone" class="h-auto w-full sm:w-2/3"/>
 	<img id="two-img" src={multideviceImg} alt="txtodo running on an iPhone, iPad, and Macbook"/>
 	<section id="mac two-text">
 		<h2 class="text-xs lg:text-lg text-center lg:text-left">
-			txtodo v2.0 brings your tasks to the cloud! now you can install txtodo on your phone and computer for a seamless cross-platform experience.
+			txtodo v3 brings your tasks to the cloud! install txtodo on your phone and computer for a seamless cross-platform experience.
 		</h2>
 		<h3 class="text-xs lg:text-lg text-center lg:text-left">
 			your tasks are secured with icloud end-to-end encryption, so your todo stays just that: yours.
@@ -53,4 +52,4 @@
 		<p>feel free to reach out to me with feedback, appreciation, or just to chat at <a href="mailto:figbert@figbert.com" rel="noopener noreferrer">figbert@figbert.com</a>.</p>
 	</div>
 </section>
-<footer id="privacy" class="mb-2 text-center" style="color: var(--subtext-color); font-size: .5rem;">txtodo does not track you. the only app analytics i receive are automatically provided by the app store. this site uses <a href="https://www.goatcounter.com/">goatcounter</a> for anonymous analytics.</footer>
+<footer id="privacy" class="mb-2 text-center" style="color: var(--subtext-color); font-size: .5rem;">txtodo does not track you. the only app analytics i receive are built in to the app store. this site uses <a href="https://umami.is/">self-hosted anonymous analytics</a>.</footer>
